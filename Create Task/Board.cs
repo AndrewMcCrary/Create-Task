38f1e8a02b31652b9ed36cd14ff54d7e9b45f585
AndrewMcFlurry <ajmccrary03@gmail.com>
2020-05-11 23:00:07 -0400

    Add project files.

---
+++ Create Task/Board.cs
@@ -0,0 +1,29 @@
+﻿namespace CreateTask
+{
+    public class Board
+    {
+        const int BOARD_X = 20;
+        const int BOARD_Y = 30;
+        Cell food;
+
+
+        Cell[,] cells = new Cell[BOARD_X, BOARD_Y];
+
+
+    }
+
+    public struct Cell
+    {
+        public int X { get; }
+        public int Y { get; }
+        public Cell(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+    }
+
+
+
+}
---
+++ Create Task/Snake.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+
+namespace CreateTask
+{
+    public class Snake
+    {
+        //starting conditions
+        const direction STARTINGDIRECTION = direction.left;
+        const int STARTINGX = 20;
+        const int STARTINGY = 15;
+        const int STARTINGLENGTH = 5;
+        const int REFRESHRATE = 2; //Cells per second
+
+        public List<snake> body = new List<snake>();
+
+        public void start()
+        {
+            body.Add(new snake(STARTINGX, STARTINGY, STARTINGDIRECTION));
+            while (body.Count <= STARTINGLENGTH - 1)
+            {
+                addLength(body[body.Count - 1]);
+            }
+
+        }
+
+        public void addLength(snake last)
+        {
+            switch (last.Direction)
+            {
+                case direction.up:
+                    body.Add(new snake(last.X, last.Y - 1, last.Direction));
+                    break;
+
+                case direction.left:
+                    body.Add(new snake(last.X + 1, last.Y, last.Direction));
+                    break;
+
+                case direction.right:
+                    body.Add(new snake(last.X - 1, last.Y, last.Direction));
+                    break;
+
+                case direction.down:
+                    body.Add(new snake(last.X, last.Y + 1, last.Direction));
+                    break;
+            }
+        }
+
+        public void refresh()
+        {
+            for (int i = 0; body.Count > i; i++)
+            {
+                switch (body[i].Direction)
+                {
+                    case direction.up:
+                        body[i].Y++;
+                        break;
+
+                    case direction.left:
+                        
+                        break;
+
+                    case direction.right:
+                        
+                        break;
+
+                    case direction.down:
+                        
+                        break;
+                }
+
+            }
+
+        }
+
+        public struct snake
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public direction Direction { get; set; }
+
+            public snake(int x, int y, direction d)
+            {
+                this.X = x;
+                this.Y = y;
+                this.Direction = d;
+            }
+
+            public void move()
+            {
+                switch (this.Direction)
+                {
+                    case direction.up:
+                        
+                        break;
+
+                    case direction.left:
+
+                        break;
+
+                    case direction.right:
+
+                        break;
+
+                    case direction.down:
+
+                        break;
+                }
+
+            }
+
+        }
+        public enum direction
+        {
+            up = 0,
+            right = 1,
+            left = 2,
+            down = 3
+
+        }
+
+
+
+
+    }
+}
